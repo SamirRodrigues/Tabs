3da098aca2cea2982dcadebf28bb8129c33b75c7
SamirRodrigues <emaildesamir@gmail.com>
2021-12-01 22:56:07 -0300

    Updates

---
+++ precache-manifest.a1c8360de52929f8d530873094a5516c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891fcae431546ba7e6a96818fa8753e4",
+    "url": "/Tabs/index.html"
+  },
+  {
+    "revision": "ba5ab3d6ca566b2422d9",
+    "url": "/Tabs/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "61c9a72a86221f692a54",
+    "url": "/Tabs/static/js/2.611ee867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tabs/static/js/2.611ee867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5ab3d6ca566b2422d9",
+    "url": "/Tabs/static/js/main.b78044ca.chunk.js"
+  },
+  {
+    "revision": "3f65111535c2ba530dce",
+    "url": "/Tabs/static/js/runtime-main.254518c6.js"
+  }
+]);
